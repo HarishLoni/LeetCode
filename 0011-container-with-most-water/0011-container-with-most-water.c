b81df9db91741df2b8254346f70f495fca1e7679
HarishLoni <163426520+HarishLoni@users.noreply.github.com>
2024-04-09 12:37:41 +0530

    Time: 59 ms (93.16%), Space: 11.4 MB (75.77%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -0,0 +1,19 @@
+int maxArea(int* height, int heightSize) {
+    
+    int left=0;
+    int right=heightSize-1;
+    int maxarea=0;
+    
+    while(left<right){
+        int area=(height[left]<height[right] ? height[left] : height[right]) * (right-left);
+        maxarea=(area>maxarea)?area:maxarea;
+        
+        if(height[left]<height[right]){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+    return maxarea;
+}
