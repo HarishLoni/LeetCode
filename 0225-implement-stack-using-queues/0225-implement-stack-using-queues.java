612fc1d3f2dc10e5749ef2152c8fc7d2c175b19d
HarishLoni <163426520+HarishLoni@users.noreply.github.com>
2024-04-11 21:07:20 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (74.65%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -13,10 +13,16 @@ class MyStack {
     }
     
     public int pop() {
+        if(queue.isEmpty()){
+            return -1;
+        }
         return queue.remove();
     }
     
     public int top() {
+        if(queue.isEmpty()){
+            return -1;
+        }
         return queue.peek();
     }
     
@@ -25,6 +31,51 @@ class MyStack {
     }
 }
 
+/*
+
+class MyStack {
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+      
+    public MyStack() {
+        queue1=new LinkedList<>();
+        queue2=new LinkedList<>();
+    }
+    
+    public void push(int x) {
+       queue1.add(x);
+    }
+    
+    public int pop() {
+       for(int i=1;i<queue1.size();i++){
+           queue2.add(queue1.remove());
+       }
+       int a=queue1.remove();
+       while(!queue2.isEmpty){
+            queue1.add(queue.remove());
+       }
+       return a;
+    }
+    
+    public int top() {
+       for(int i=1;i<queue1.size();i++){
+           queue2.add(queue1.remove());
+       }
+       int a=queue1.peek();
+       queue2.add(queue1.remove());
+       while(!queue2.isEmpty()){
+            queue1.add(queue2.remove());
+       }
+       return a;
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+
+*/
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack obj = new MyStack();
