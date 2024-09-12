84c333bbcf1536fd1338ae908a7a16ab0549f951
HarishLoni <163426520+HarishLoni@users.noreply.github.com>
2024-09-12 19:38:30 +0530

    Time: 2 ms (75.55%), Space: 45.8 MB (22.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,27 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxfind=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxfind){
-                return false;
-            }
-            maxfind=Math.max(maxfind,i+nums[i]);
+        // int maxfind=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(i>maxfind){
+        //         return false;
+        //     }
+        //     maxfind=Math.max(maxfind,i+nums[i]);
+        // }
+        // return true;
+        
+        
+        int max=0;
+        int n=nums.length;
+        
+        if(n==1){
+            return true;
         }
-        return true;
+        for(int i=0;i<nums.length-1 && max>=i ;i++){
+            max=Math.max(max,i+nums[i]);
+            if(max>=n-1){
+                return true;
+            }
+        }
+        return false;
     }
 }
