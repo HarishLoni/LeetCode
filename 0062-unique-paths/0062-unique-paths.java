8cf09cdc6c18bac01e8e3e4a16ecdd21a87b201b
Harish Loni <163426520+HarishLoni@users.noreply.github.com>
2024-11-10 21:23:34 +0530

    Time: 1 ms (9.38%), Space: 40.9 MB (5.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,25 +5,45 @@ class Solution {
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        
+        dp[0][0]=1;
         int res=solve(dp,m-1,n-1);
         return res;
     }
     
+//     public static int solve(int dp[][],int m,int n){
+//         if(m==0 && n==0){
+//             return 1;
+//         }
+//         if(m<0 || n<0){
+//             return 0;
+//         }
+//         if(dp[m][n]!=-1){
+//             return dp[m][n];
+//         }
+        
+//         int left=solve(dp,m-1,n);
+//         int up=solve(dp,m,n-1);
+        
+//         return dp[m][n]=left+up;
+//     }
+    
     public static int solve(int dp[][],int m,int n){
-        if(m==0 && n==0){
-            return 1;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0){
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
+            }
         }
-        if(m<0 || n<0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        
-        int left=solve(dp,m-1,n);
-        int up=solve(dp,m,n-1);
-        
-        return dp[m][n]=left+up;
+        return dp[m][n];
     }
 }
