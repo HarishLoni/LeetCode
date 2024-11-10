88f5004de211185ceb46922e8afd94e714346356
Harish Loni <163426520+HarishLoni@users.noreply.github.com>
2024-11-10 21:59:58 +0530

    Time: 1 ms (21.88%), Space: 41.5 MB (67.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,28 +1,67 @@
 class Solution {
-    int dp[][];
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int dp[][]=new int[m][n];
         
-        dp=new int[obstacleGrid.length][obstacleGrid[0].length];
-        
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        
-        return dfs(obstacleGrid,0,0);
-    }
-    
-    private int dfs(int[][] obstacleGrid,int row,int col){
-        if(row<0 || col<0 || row==obstacleGrid.length || col==obstacleGrid[0].length || obstacleGrid[row][col]==1){
+        if(obstacleGrid[0][0]==1){
             return 0;
         }
-        if(row==obstacleGrid.length-1 && col==obstacleGrid[0].length-1){
-            return 1;
+        dp[0][0]=1;
+        
+        int res=solve(obstacleGrid,dp,m-1,n-1);
+        return res;
+    }
+    
+//         public static int solve(int[][] obstacleGrid,int dp[][],int m,int n){
+//         if(m==0 && n==0){
+//             return 1;
+//         }
+//         if(m<0 || n<0){
+//             return 0;
+//         }
+//         if(obstacleGrid[m][n]==1){
+//             return 0;
+//         }
+
+
+//         if(dp[m][n]!=-1){
+//             return dp[m][n];
+//         }
+        
+//         int left=solve(obstacleGrid,dp,m-1,n);
+//         int up=solve(obstacleGrid,dp,m,n-1);
+        
+//         return dp[m][n]=left+up;
+//     }
+// }
+    
+    public static int solve(int[][] obstacleGrid,int dp[][],int m,int n){
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0){
+                    continue;
+                }
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
+            }
         }
+        return dp[m][n];
         
-        if(dp[row][col]>-1) return dp[row][col];
-        
-        dp[row][col]=dfs(obstacleGrid,row+1,col)+dfs(obstacleGrid,row,col+1);
-        
-        return dp[row][col];
     }
 }
+
