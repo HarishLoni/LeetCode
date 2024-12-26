1304ac959798526909b550031103b2940a7fa458
Harish Loni <163426520+HarishLoni@users.noreply.github.com>
2024-12-26 10:47:56 +0530

    Time: 1 ms (99.85%), Space: 53.4 MB (15.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,22 +4,39 @@ class Solution {
         // Arrays.sort(nums);
         // return nums[n];
         
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int m=(nums.length)/2;
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         int m=(nums.length)/2;
+//         for(int i=0;i<nums.length;i++){
+//             if(map.containsKey(nums[i])){
+//                 map.put(nums[i],map.get(nums[i])+1);
+//             }
+//             else{
+//                 map.put(nums[i],1);
+//             }
+//         }
+        
+//         for(int k:map.keySet()){
+//             if(map.get(k)>m){
+//                 return k;
+//             }
+//         }
+//         return 0;
+        
+        int n=nums.length;
+        int ele=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                count=1;
+                ele=nums[i];
+            }
+            else if(nums[i]==ele){
+                count++;
             }
             else{
-                map.put(nums[i],1);
+                count--;
             }
         }
-        
-        for(int k:map.keySet()){
-            if(map.get(k)>m){
-                return k;
-            }
-        }
-        return 0;
+        return ele;
     }
 }
